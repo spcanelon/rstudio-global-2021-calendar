608877f7e2d3f7b20845cd518ac353d7bbee0d5d
spcanelon <sp.canelon@protonmail.com>
2021-02-01 20:47:27 -0500

    Save get_dates function

---
+++ R/get_dates.R
@@ -0,0 +1,9 @@
+get_dates <- function(page_number) {
+  Sys.sleep(2)
+
+  link <- paste0("https://global.rstudio.com/student/all_events?page=", page_number)
+
+  rvest::read_html(link) %>%
+    rvest::html_nodes(".session__dates.session__dates--index") %>%
+    rvest::html_text()
+}
